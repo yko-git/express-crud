b84c832557424d0da435b2cf0828c386faf3e1bb
yko-git <yko.iwata@gmail.com>
2024-07-14 11:08:02 +0900

    models/indexファイルをts化（develop）

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,15 +1,14 @@
 import path from "path";
 import { Sequelize } from "sequelize";
-
-const env = process.env.NODE_ENV || "development";
-const config = require(path.join(__dirname, "/../config/config.json"))[env];
-
-export const sequelize = new Sequelize(config);
-
 import Users from "./users";
 import Categories from "./categories";
 import PostCategories from "./postCategories";
 import Posts from "./posts";
+
+const env = process.env.NODE_ENV || "development";
+const config = require(path.join(__dirname, "/../config/config.json"))[env];
+
+const sequelize = new Sequelize(config);
 
 const models = {
   Users,
