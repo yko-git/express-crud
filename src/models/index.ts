24e4fb36728dd669876837b43dfac3650e95191b
yko-git <yko.iwata@gmail.com>
2024-07-14 10:42:58 +0900

    models/indexファイルをts化（develop）

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,48 +1,23 @@
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const process = require("process");
-const basename = path.basename(__filename);
+import path from "path";
+import { Sequelize } from "sequelize";
+
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
-const db = {};
+const config = require(path.join(__dirname, "/../config/config.json"))[env];
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+export const sequelize = new Sequelize(config);
 
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 &&
-      file !== basename &&
-      file.slice(-3) === ".ts" &&
-      file.indexOf(".test.ts") === -1
-    );
-  })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
-    db[model.name] = model;
-  });
+import Users from "./users";
+import Categories from "./categories";
+import PostCategories from "./postCategories";
+import Posts from "./posts";
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const models = {
+  Users,
+  Categories,
+  PostCategories,
+  Posts,
+};
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+export type Models = typeof models;
 
-module.exports = db;
+export default models;
