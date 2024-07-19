1c8c617715bac34c89f759525869097f888e268e
yko-git <yko.iwata@gmail.com>
2024-07-19 18:04:04 +0900

    createの処理（検証中）（develop）

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
 import express, { Request, Response } from "express";
+const bodyParser = require("body-parser");
 
 const app = express();
+const models = require("./models");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send({ message: "ok" });
 });
 
-app.listen(3000, () => {
-  console.log("リクエストをポート3000で待受中");
+app.listen(3000);
+console.log("Server is online..");
+
+app.post("/auth/signup", async (req: Request, res: Response) => {
+  try {
+    const user = await models.Users.create({
+      loginId: "hoge@example.com",
+      name: "hoge",
+      iconUrl: "http://localhost",
+      authorize_token: "password",
+    });
+    console.log(user.toJSON());
+  } catch (error) {
+    console.log(error);
+  }
 });
