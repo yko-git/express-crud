57877f749cedbfbf4171bb9835b91222c6f5856f
yko-git <yko.iwata@gmail.com>
2024-07-21 10:57:18 +0900

    createの処理・req.bodyから値を取得（develop）

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import User from "./models/user";
+import bodyParser from "body-parser";
+// import passport from "./auth";
 
 const app = express();
 
@@ -7,18 +9,25 @@ app.get("/", (req: Request, res: Response) => {
   res.send({ message: "ok" });
 });
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
 app.listen(3000);
-console.log("Server is online..");
+console.log("Server is online.");
 
 app.post("/auth/signup", async (req: Request, res: Response) => {
   try {
     const user = await User.create({
-      loginId: "hoge@example.com",
-      name: "hoge",
-      iconUrl: "http://localhost",
-      authorize_token: "password",
+      loginId: req.body.user.loginId,
+      name: req.body.user.name,
+      iconUrl: req.body.user.iconUrl,
+      authorize_token: req.body.user.authorize_token,
     });
-    console.log(user.toJSON());
+    console.log(`req.body:${req.body}`);
   } catch (error) {
     console.log(error);
   }
