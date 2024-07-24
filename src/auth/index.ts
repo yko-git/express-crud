a2f2a67a1fa18fc995c7f100ef866c8feadd682c
yko-git <yko.iwata@gmail.com>
2024-07-24 20:59:37 +0900

    ログイン時に受け取ったパスワードをハッシュ化+ソルト+ペッパーにしてから条件分岐（検証中）（develop）

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from "express";
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import User from "../models/user";
+import bcrypt from "bcrypt";
 
 const app = express();
 
@@ -10,27 +11,31 @@ passport.use(
   new LocalStrategy(
     {
       usernameField: "loginId",
-      passwordField: "authorizeToken",
+      passwordField: "password",
       session: false,
     },
-    (loginId: string, authorizeToken: string, done: any) => {
-      User.findOne({
-        where: { loginId: loginId, authorizeToken: authorizeToken },
-      })
-        .then((user) => {
-          if (user) {
-            return done(null, loginId);
+    async (loginId: string, password: string, done: any) => {
+      const user = await User.findOne({
+        where: { loginId: loginId, authorizeToken: password },
+      });
+      let hashedPassword = await bcrypt.hash(
+        `${password}${process.env.MYPEPPER}`,
+        10
+      );
+      try {
+        if (user) {
+          if (user.dataValues.authorizeToken === hashedPassword) {
+            return done(null, loginId, {
+              message: "ユーザーID・パスワードが正しく認証されました。",
+            });
           }
-
-          throw new Error();
-        })
-        .catch((error) => {
-          // エラー処理
-
-          return done(null, false, {
-            message: "認証情報と一致するレコードがありません。",
-          });
+        }
+        throw new Error();
+      } catch (error) {
+        return done(null, false, {
+          message: "認証情報と一致するレコードがありません。",
         });
+      }
     }
   )
 );
