c44ea48b55b29ff35bb537555e5c7fa5f36ec548
yko-git <yko.iwata@gmail.com>
2024-07-24 15:50:07 +0900

    .env.JWT_SECRETが設定されていなかった場合アプリを開始させない処理追加（develop）

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import bcrypt from "bcrypt";
 
 const app = express();
 
-if (process.env.MYPEPPER) {
+if (process.env.MYPEPPER && process.env.JWT_SECRET) {
   app.listen(3000);
   console.log("Server is online.");
 }
@@ -30,6 +30,7 @@ app.post("/auth/signup", async (req: Request, res: Response) => {
       `${req.body.user.password}${process.env.MYPEPPER}`,
       10
     );
+    console.log(`signup:${hashedPassword}`);
     const user = await User.create({
       loginId: req.body.user.loginId,
       name: req.body.user.name,
@@ -37,8 +38,6 @@ app.post("/auth/signup", async (req: Request, res: Response) => {
       authorizeToken: hashedPassword,
     });
   } catch (error) {
-    console.log(error);
-
     res.json({ error });
   }
 });
@@ -47,15 +46,23 @@ app.post("/auth/signup", async (req: Request, res: Response) => {
 app.use(passport.initialize());
 
 app.post(
-  "/auth/signin",
+  "/auth/login",
   passport.authenticate("local", { session: false }),
   (req: Request, res: Response, next) => {
-    // jwtのtokenを作成
-    const user = req.user;
-    const payload = { user: req.user };
-    const token = jwt.sign(payload, `${process.env.JWT_SECRET}` as string, {
-      expiresIn: "1m",
-    });
-    res.json({ user, token });
+    try {
+      // jwtのtokenを作成
+      const user = req.user;
+      const payload = { user: req.user };
+      const token = jwt.sign(payload, `${process.env.JWT_SECRET}` as string, {
+        expiresIn: "1m",
+      });
+      res.json({ user, token });
+    } catch (error) {
+      return res.status(401).json([
+        {
+          message: "認証ができませんでした。",
+        },
+      ]);
+    }
   }
 );
