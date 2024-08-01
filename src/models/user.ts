8d827d05a6eebf741b6423817f7ca7535bf1b041
yko-git <yko.iwata@gmail.com>
2024-08-01 17:19:08 +0900

    userIdを生成できない問題でAssociationsを修正の検証（develop）

--- src/models/user.ts
+++ src/models/user.ts
@@ -33,12 +33,7 @@ User.init(
   }
 );
 
-User.hasMany(Post, {
-  foreignKey: "userId",
-});
-
-Post.belongsTo(User, {
-  foreignKey: "userId",
-});
+User.hasMany(Post, { foreignKey: "userId" });
+Post.belongsTo(User);
 
 export default User;
