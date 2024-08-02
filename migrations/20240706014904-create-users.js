edc4253975b46cf0a23e4cbf10ec06ee982c645c
yko-git <yko.iwata@gmail.com>
2024-08-02 18:07:08 +0900

    users・postsにvalidationを設定（develop）

--- migrations/20240706014904-create-users.js
+++ migrations/20240706014904-create-users.js
@@ -4,22 +4,36 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("users", {
       id: {
-        allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
       loginId: {
         type: Sequelize.STRING,
+        validate: {
+          notNull: true,
+        },
       },
       authorizeToken: {
         type: Sequelize.STRING,
       },
       name: {
         type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "名前は必ず入力してください",
+          },
+        },
       },
       iconUrl: {
         type: Sequelize.TEXT,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "iconUrlは必ず入力してください",
+          },
+        },
       },
       createdAt: {
         type: Sequelize.DATE,
--- migrations/20240706015124-create-posts.js
+++ migrations/20240706015124-create-posts.js
@@ -4,22 +4,42 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("posts", {
       id: {
-        allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
       userId: {
         type: Sequelize.INTEGER,
+        validate: {
+          notNull: true,
+        },
       },
       title: {
         type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "titleは必ず入力してください",
+          },
+        },
       },
       body: {
         type: Sequelize.TEXT,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "bodyは必ず入力してください",
+          },
+        },
       },
       status: {
         type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "statusは必ず入力してください",
+          },
+        },
       },
       createdAt: {
         type: Sequelize.DATE,
