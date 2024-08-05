597c3558daf3ca7d2fad0fbac9ddc2859dc1a8b8
yko-git <yko.iwata@gmail.com>
2024-08-05 21:01:23 +0900

    models/postCategoryの型の修正（develop）

--- src/models/postCategory.ts
+++ src/models/postCategory.ts
@@ -4,22 +4,30 @@ import {
   InferAttributes,
   InferCreationAttributes,
   CreationOptional,
+  ForeignKey,
 } from "sequelize";
 
+import Post from "./post";
 import { sequelize } from ".";
+import Category from "./category";
 
 class PostCategory extends Model<
   InferAttributes<PostCategory>,
   InferCreationAttributes<PostCategory>
 > {
-  declare postId: number;
-  declare categoryId: number;
+  declare id: CreationOptional<number>;
+  declare postId: ForeignKey<Post["id"]>;
+  declare categoryId: ForeignKey<Category["id"]>;
   declare createdAt: CreationOptional<Date>;
   declare updatedAt: CreationOptional<Date>;
 }
 
 PostCategory.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
     postId: DataTypes.INTEGER,
     categoryId: DataTypes.INTEGER,
     createdAt: DataTypes.NOW,
